58ee97e197a1e8826eafc984fb070015eb5fa93b
Arthur Milchior <Arthur@Milchior.fr>
2020-05-19 01:47:49 +0200

    Solving #4

--- __init__.py
+++ __init__.py
@@ -23,6 +23,7 @@ def note_loaded(editor):
     ]
     dumped = json.dumps(fldContentTexProcessed)
     editor.web.eval(f"""set_texs({dumped});""")
+    editor.web.eval(f"on_focus_field(0);")
 
 
 gui_hooks.editor_did_load_note.append(note_loaded)
